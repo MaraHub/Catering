e829e252aab85f9c618f9b5bb1fa327cab901668
Your Name <you@example.com>
2018-12-17 00:50:54 +0100

    first version with BS

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/mainpage')
+def main():
+    return render_template('index.html',kyriws = ['Patatosalata','Gemista','Tzatzikara'])
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html',kyriws = {'Patatosalata',
+                                                'Gemista',
+                                                'Tzatzikara',
+                                                'Makaronia',
+                                                'Rizoto'})
+
+## Run the App
+if __name__ == '__main__':
+  app.run(debug=True)
